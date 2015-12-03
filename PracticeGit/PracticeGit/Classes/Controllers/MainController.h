18c9b6b133ffd4a42cfc5cbf9a5544f21466d044
March <march830830@gmail.com>
2015-12-03 21:48:35 +0800

    first

---
+++ PracticeGit/PracticeGit/Classes/Controllers/MainController.h
@@ -0,0 +1,13 @@
+//
+//  MainController.h
+//  PracticeGit
+//
+//  Created by 陳泓諺 on 2015/12/3.
+//  Copyright © 2015年 陳泓諺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface MainController : UIViewController
+
+@end
---
+++ PracticeGit/PracticeGit/Classes/Views/MainView.m
@@ -0,0 +1,21 @@
+//
+//  MainView.m
+//  PracticeGit
+//
+//  Created by 陳泓諺 on 2015/12/3.
+//  Copyright © 2015年 陳泓諺. All rights reserved.
+//
+
+#import "MainView.h"
+
+@implementation MainView
+
+/*
+// Only override drawRect: if you perform custom drawing.
+// An empty implementation adversely affects performance during animation.
+- (void)drawRect:(CGRect)rect {
+    // Drawing code
+}
+*/
+
+@end

635e277af2df4125fe444425952a1daeb6fe496b
March <march830830@gmail.com>
2015-12-03 21:44:43 +0800

    first commit

---
+++ PracticeGit/PracticeGit/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  PracticeGit
+//
+//  Created by 陳泓諺 on 2015/12/3.
+//  Copyright © 2015年 陳泓諺. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+

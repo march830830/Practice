ec9f2fd0bb274ae6e59715854702403fb4826914
March <march830830@gmail.com>
2015-12-03 21:50:53 +0800

    controller

--- PracticeGit/PracticeGit/AppDelegate.h
+++ PracticeGit/PracticeGit/AppDelegate.h
@@ -7,11 +7,12 @@
 //
 
 #import <UIKit/UIKit.h>
+@class MainController;
 
 @interface AppDelegate : UIResponder <UIApplicationDelegate>
 
 @property (strong, nonatomic) UIWindow *window;
-
+@property (nonatomic, strong) MainController *mainController;
 
 @end
 
